097d93675f4cecf2be60ce58b7d410ae06e7c37c
Pranav <135952213+Pranav-rkpp@users.noreply.github.com>
2024-06-22 19:26:03 +0530

    route structure

--- main.js
+++ main.js
@@ -3,12 +3,35 @@ import express from 'express';
 const app = express();
 const PORT = 6969;
 
-app.get('/',(req,res)=>{
-    res.json({message:"Hello Students"})
+app.get('/', (req, res) => {
+    res.json({ message: "Hello Students" })
 });
 
-app.listen(PORT,()=>{
-console.log(`the server is running at http://localhost:${PORT}`)
+//CRUD functionality
+
+//C - create movies
+app.post('/movies', () => {
+
+})
+
+//R - Read movies
+app.get('/movies', () => {
+
+})
+
+//U - Update movies
+app.put('/movies/:id', () => {
+
+})
+
+//D - Delete movies
+app.delete('/movies/:id', () => {
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`the server is running at http://localhost:${PORT}`)
 })
 
 //DRY Principle- Don't Repeat Yourself

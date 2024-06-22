a2ab6cc4d77aa5e65f057c90d647f9e7b10103c5
Pranav <135952213+Pranav-rkpp@users.noreply.github.com>
2024-06-22 21:48:01 +0530

    add model

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,15 +1,31 @@
 import { response } from "express";
+import Movie from "../models/movie.model.js";
 
 export const MovieIndex = (req, res) => {
     res.send("Get all movie lists");
 };
 
-export const MovieCreate = (req, res) => {
+export const MovieCreate = async (req, res) => {
     //Create the movie info.
     //Structure - id, title, desc
-    console.log(req.body);
+    // console.log(req.body);
 
-    return res.json(req.body);  //Need to add data understanding middleware app.use(express.json())
+    //Validate your data. Never trust user
+
+    const newMovie = new Movie({
+        title: req.body.title,
+        desc: req.body.desc
+    });
+
+    //Successful or error
+    try {
+        const movie = await newMovie.save();
+        return res.status(201).json(movie);
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+
+    // return res.json(req.body);  //Need to add data understanding middleware app.use(express.json())
 };
 
 export const MovieUpdate = (req, res) => {
